fc697877cb02065815884f7bec8fce18d1dbc145
Tomasz Skorupski <skorupa7@o2.pl>
2026-04-12 01:28:46 +0200

    fix: force startup.sh in Azure deployment part 2

--- startup.sh
+++ startup.sh
@@ -1,2 +1,7 @@
-cd /home/site/wwwroot
-gunicorn --bind 0.0.0.0:8000 --workers 1 --threads 2 --timeout 30 app:app
+gunicorn --bind 0.0.0.0:8000 \
+         --workers 1 \
+         --threads 2 \
+         --timeout 30 \
+         --chdir /home/site/wwwroot \
+         --pythonpath /home/site/wwwroot \
+         app:app
